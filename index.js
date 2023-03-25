ff815f2084eb1dc8ff6f83542e4ced9790b7924e
Gulzar Ali <gulzar.ali@renesistech.com>
2023-03-25 11:14:12 +0500

    yotpo react: product reviews count

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
 
 const YotpoWidget = ({
   productId,
@@ -7,23 +9,38 @@ const YotpoWidget = ({
   productImageUrl,
   productPrice,
   productCurrency,
-  apiKey
+  apiKey,
 }) => {
+  const [error, setError] = useState(null);
+  const router = useRouter();
+
   useEffect(() => {
+    if (!productId) {
+      setError("Missing productId");
+      return;
+    }
     const script = document.createElement("script");
     script.type = "text/javascript";
     script.async = true;
-    script.src =
-      `//staticw2.yotpo.com/${apiKey}/widget.js`;
+    script.src = `//staticw2.yotpo.com/${apiKey}/widget.js`;
     const firstScript = document.getElementsByTagName("script")[0];
     firstScript.parentNode.insertBefore(script, firstScript);
-  }, []);
+  }, [router]);
+  if (typeof window['yotpo'] !== 'undefined') {
+    window['yotpo'].initWidgets();
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <div
       className="yotpo yotpo-main-widget"
+      data-product-id={productId}
       data-id={productId}
       data-sku={productId}
+      data-product-sku={productId}
       data-name={productName}
       data-url={productUrl}
       data-image-url={productImageUrl}
@@ -31,6 +48,28 @@ const YotpoWidget = ({
       data-currency={productCurrency}
     ></div>
   );
+};
+
+export const YotpoProductReviewStates = ({ productId, apiKey }) => {
+  const [reviewsCount, setReviewsCount] = useState({
+    average_score: 0,
+    organic_average_score: 0,
+    total_organic_reviews: 0,
+    total_review: 0,
+  });
+
+  useEffect(() => {
+    fetch(
+      `https://api.yotpo.com/v1/widget/${apiKey}/products/${productId}/bottomline`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setReviewsCount(data.response.bottomline);
+      })
+      .catch((error) => console.error(error));
+  }, [productId, apiKey]);
+
+  return reviewsCount;
 };
 
 export default YotpoWidget;
